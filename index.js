4f2205486a38fcb2158daaa1d1269266700fc4e1
Rotich <jane.rotich@student.moringaschool.com>
2023-12-15 21:05:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+function getRandomInteger() {
+    return Math.floor(Math.random() * 10);
+  }
+const random = getRandomInteger();
+
+Math.random();
+let num3 = 24; 
+let num4 = 5;
+const mod = num3 % num4;
+
+let numbers = (17, 9, 20, 11);
+const max = Math.max(17, 9, 20, 11);
+
+
